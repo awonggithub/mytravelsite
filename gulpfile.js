d5d6428a670bd534d2f5e9c90073555ba2547c9f
Andy <awong_mailbox@yahoo.com.au>
2017-03-31 16:52:02 +1100

    Completed automatic sprite generation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 /* This is the master file for gulpfile that will call other gulp files */
 require('./gulp/tasks/styles');   /* when using require, you don't needed to specify .css */
 require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
